b34d5aa70a57766c58dfe32a3c5eb1ebad819e0e
Udayskn <2021mcb1253@iitrpr.ac.in>
2023-07-09 12:10:00 +0530

    LCS

---
+++ SS2_LCS.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>>dp;
+string arr1,arr2;
+int LCS(int i,int j){
+    if(i==-1 ||j==-1)return 0;
+    if(dp[i][j]!=-1)return dp[i][j];
+    if(arr1[i]==arr2[j])return dp[i][j]=1+LCS(i-1,j-1);
+    return dp[i][j]= max(LCS(i,j-1),LCS(i-1,j)); 
+}
+int main(){
+    cin>>arr1>>arr2;
+    int m=arr1.size(),n=arr2.size();
+    dp.resize(m,vector<int>(n,-1));
+    cout<<LCS(m-1,n-1);
+}
