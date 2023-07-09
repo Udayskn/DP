3de80cf551719312690fc9cceddd18cfa00d71c7
Udayskn <115459469+Udayskn@users.noreply.github.com>
2023-07-09 10:42:09 +0530

    Update Kadane3_01.cpp

--- Kadane3_01.cpp
+++ Kadane3_01.cpp
@@ -9,12 +9,14 @@ int main() {
   dpl[0]=arr[0];
   dpr[n-1]=arr[n-1];
   int k=dpl[0];
+  // running kandanes from both ends and cal normal kadane
   for(int i=1;i<n;i++){
     dpl[i]=max(arr[i],dpl[i-1]+arr[i]);
     k=max(dpl[i],k);
     int j=n-1-i;
     dpr[j]=max(arr[j],dpr[j+1]+arr[j]);
   }
+  // checking kadane if ith element is deleted.
   for(int i=1;i<n-1;i++){
     int j=n-1-i;
     k=max(k,dpl[i-1]+dpr[i+1]);
