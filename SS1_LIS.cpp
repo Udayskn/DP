4e3dcea4768480a0e6b6ae8ed5f1930267e9d2b2
Udayskn <115459469+Udayskn@users.noreply.github.com>
2023-08-30 23:05:41 +0530

    Update SS1_LIS.cpp

--- SS1_LIS.cpp
+++ SS1_LIS.cpp
@@ -9,7 +9,7 @@ int main() {
   for(int i=1;i<n;i++){
     for(int j=0;j<i;j++){
       // for lis check if arr[i]>arr[j]
-      if(arr[i]%arr[j]==0){
+      if(arr[i]%arr[j]==0){                                 //longest dividing subsequence
         dp[i]=max(dp[i],dp[j]+1);
       }
     }
